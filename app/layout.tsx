f2c3f47e3c5a7f5a1f920b87abf0a6309bf5b6d1
rollerminingdot-max <rollerminingdot@gmail.com>
2026-01-01 20:49:28 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,48 +2,54 @@
 
 /**
  * This project was developed by Nikandr Surkov.
- * 
+ *
  * YouTube: https://www.youtube.com/@NikandrSurkov
  * GitHub: https://github.com/nikandr-surkov
  */
 
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import Script from 'next/script'
+import './globals.css'
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
+    src: './fonts/GeistVF.woff',
+    variable: '--font-geist-sans',
+    weight: '100 900'
+})
+
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+    src: './fonts/GeistMonoVF.woff',
+    variable: '--font-geist-mono',
+    weight: '100 900'
+})
 
 export const metadata: Metadata = {
-  title: "Paws Game Clone",
-  description: "Clone of the popular Telegram mini app Paws, developed by Nikandr Surkov.",
-};
+    title: 'Paws Game Clone',
+    description:
+        'Clone of the popular Telegram mini app Paws, developed by Nikandr Surkov.'
+}
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <script
-  src="//libtl.com/sdk.js"
-  data-zone="10404224"
-  data-sdk="show_10404224"
-></script>
-      </body>
-    </html>
-  );
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+
+                {/* Monetag SDK */}
+                <Script
+                    src="//libtl.com/sdk.js"
+                    strategy="afterInteractive"
+                    data-zone="10404224"
+                    data-sdk="show_10404224"
+                />
+            </body>
+        </html>
+    )
 }
