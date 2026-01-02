8ad53d162fbc4244d616dda42dde5611db2e33f6
rollerminingdot-max <rollerminingdot@gmail.com>
2026-01-02 03:20:34 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   trailingSlash: true,
-
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 }
 
 export default nextConfig 
